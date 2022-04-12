2a963a45bb3b5caa471dcb4ace60f851312a96ef
Muhammad Febriansyah <muh.febriansyah@gmail.com>
2022-04-12 16:26:49 +0700

    Initialize project

---
+++ logical-test/logical-test/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using logical_test;
+
+Console.WriteLine("****************** Palindrome test ******************");
+Console.WriteLine("Please enter your word below (type exit for close the program)");
+
+logicals objLogicals = new logicals();
+string strInput = Console.ReadLine();
+
+retry:
+if (strInput.ToLower() != "exit")
+{
+    Console.WriteLine("Word: " + strInput);
+    Console.WriteLine("Is palindrome: " + objLogicals.IsPalindrome(strInput.ToLower().Replace(" ", "")));
+    Console.WriteLine("");
+    Console.WriteLine("type another word");
+    strInput = Console.ReadLine();
+    goto retry;
+}
---
+++ logical-test/logical-test/logicals.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logical_test
+{
+    internal class logicals
+    {
+        public bool IsPalindrome(string pStrText)
+        {
+            int intEnd = pStrText.Length/2;
+
+            //string 1
+            string str1 = string.Empty;
+            for (int intLoop=0; intLoop<intEnd; intLoop++)
+            {
+                str1 += pStrText[intLoop];
+            }
+            _ = str1.ToLower();
+
+            //string 2
+            string str2 = string.Empty;
+            for (int intLoop = pStrText.Length-1; intLoop > intEnd; intLoop--)
+            {
+                str2 += pStrText[intLoop];
+            }
+            _ = str2.ToLower();
+
+            if (str1 == str2) { return true; } else { return false; }
+        }
+    }
+}
