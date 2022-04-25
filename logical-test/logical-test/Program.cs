4046a763abf33052ee3e611526d7471fbcee2bdc
Muhammad Febriansyah <muh.febriansyah@gmail.com>
2022-04-25 16:57:32 +0700

    Add some additional logics

--- logical-test/logical-test/Program.cs
+++ logical-test/logical-test/Program.cs
@@ -1,19 +1,81 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using logical_test;
 
-Console.WriteLine("****************** Palindrome test ******************");
-Console.WriteLine("Please enter your word below (type exit for close the program)");
+Console.WriteLine("Logical list:");
+Console.WriteLine("1. Palindrome");
+Console.WriteLine("2. Counting Triangle Stars");
+Console.WriteLine("3. Get triangle (input 3 value)");
+Console.WriteLine("");
+Console.WriteLine("Please choose:");
 
-logicals objLogicals = new logicals();
-string strInput = Console.ReadLine();
+GetOption:
+string strOption = Console.ReadLine();
+
+if (strOption == "1")
+{
+    Console.WriteLine("****************** Palindrome test ******************");
+    Console.WriteLine("Please enter your word below (type exit for close the program)");
+
+    logicals objLogicals = new logicals();
+    string strInput = Console.ReadLine();
 
 retry:
-if (strInput.ToLower() != "exit")
+    if (strInput.ToLower() != "exit")
+    {
+        Console.WriteLine("Word: " + strInput);
+        Console.WriteLine("Is palindrome: " + objLogicals.IsPalindrome(strInput.ToLower().Replace(" ", "")));
+        Console.WriteLine("");
+        Console.WriteLine("type another word");
+        strInput = Console.ReadLine();
+        goto retry;
+    }
+}
+else if (strOption == "2")
 {
-    Console.WriteLine("Word: " + strInput);
-    Console.WriteLine("Is palindrome: " + objLogicals.IsPalindrome(strInput.ToLower().Replace(" ", "")));
-    Console.WriteLine("");
-    Console.WriteLine("type another word");
-    strInput = Console.ReadLine();
-    goto retry;
+    Console.WriteLine("****************** Triangle test ******************");
+    Console.WriteLine("Please enter the number of star (type exit for close the program)");
+
+    logicals objLogicals = new logicals();
+    string strInput = Console.ReadLine();
+
+retry:
+    if (strInput.ToLower() != "exit")
+    {
+        Console.WriteLine("Star(s): " + strInput);
+        objLogicals.Stars(Convert.ToInt32(strInput));
+        Console.WriteLine("");
+        Console.WriteLine("type another star");
+        strInput = Console.ReadLine();
+        goto retry;
+    }
+}
+else if (strOption == "3")
+{
+    Console.WriteLine("****************** Get Triangle ******************");
+    Console.WriteLine("Please enter 3 number by typing OK for the first (type exit for close the program)");
+
+    string strInput = Console.ReadLine();
+retry:
+    if (strInput == "OK")
+    {
+        logicals objLogicals = new logicals();
+        Console.WriteLine("Value a : ");
+        string strInput1 = Console.ReadLine();
+        Console.WriteLine("Value b : ");
+        string strInput2 = Console.ReadLine();
+        Console.WriteLine("Value c : ");
+        string strInput3 = Console.ReadLine();
+
+        Console.WriteLine("Your input: " + strInput1 + ", " + strInput2 + ", " + strInput3);
+        Console.WriteLine(objLogicals.TriangleType(Convert.ToInt32(strInput1), Convert.ToInt32(strInput2), Convert.ToInt32(strInput3)));
+        Console.WriteLine("type OK if you wanna repeat or type anything to exit");
+        strInput = Console.ReadLine();
+        Console.WriteLine("");
+        goto retry;
+    }
+}
+else
+{
+    Console.WriteLine("Please choose the logical test");
+    goto GetOption;
 }
--- logical-test/logical-test/logicals.cs
+++ logical-test/logical-test/logicals.cs
@@ -22,13 +22,46 @@ namespace logical_test
 
             //string 2
             string str2 = string.Empty;
-            for (int intLoop = pStrText.Length-1; intLoop > intEnd; intLoop--)
+            if (pStrText.Length%2 == 0)
             {
-                str2 += pStrText[intLoop];
+                for (int intLoop = pStrText.Length - 1; intLoop >= intEnd; intLoop--)
+                {
+                    str2 += pStrText[intLoop];
+                }
             }
+            else
+            {
+                for (int intLoop = pStrText.Length - 1; intLoop > intEnd; intLoop--)
+                {
+                    str2 += pStrText[intLoop];
+                }
+            }            
             _ = str2.ToLower();
 
             if (str1 == str2) { return true; } else { return false; }
         }
-    }
+
+        public void Stars (int pIntNumber)
+        {
+            string strStars = string.Empty;
+            for (int i = 0; i < pIntNumber; i++)
+            {                
+                for (int j = 0; j <= i; j++)
+                {
+                    strStars += "*"; 
+                }
+                Console.WriteLine(strStars); strStars = "";
+            }
+        }
+
+        public enum Type { SAMA_KAKI, SAMA_SISI, IRREGULAR, BUKAN_SEGITIGA }
+        public Type TriangleType (int a, int b, int c)
+        {
+            if (a == 0 || b == 0 || c == 0) { return Type.BUKAN_SEGITIGA; }
+            else if (a == b && b == c) { return Type.SAMA_SISI; }
+            else if (a == b || a == c || b == c) { return Type.SAMA_KAKI; }
+            else if (a != b && b != c) { return Type.IRREGULAR; }
+            else { return Type.BUKAN_SEGITIGA;}
+        }
+    } 
 }
